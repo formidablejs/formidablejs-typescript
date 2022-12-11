1f58ec55f17019e2fd3cee0c399a0bf43b82ae9a
Donald Pakkies <donaldpakkies@gmail.com>
2022-12-11 13:53:18 +0200

    feat: add schedule method

--- app/Console/Kernel.ts
+++ app/Console/Kernel.ts
@@ -1,9 +1,17 @@
 import { ConsoleKernel } from '@formidablejs/framework'
+import { Log } from '@formidablejs/logger'
+import type { Schedule } from '@formidablejs/scheduler'
 
 export class Kernel extends ConsoleKernel {
 	get registered(): Array<object> {
 		return [
 			//
 		]
+	}
+
+	schedule(schedule: Schedule): void {
+		// schedule.call(() => {
+		// 	Log.info('Hello World')
+		// }).everyMinute()
 	}
 }
