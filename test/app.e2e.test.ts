7f63fc66ecaf5192e3cfc231d7f8f463b4984944
Donald Pakkies <donaldpakkies@gmail.com>
2023-11-03 15:12:52 +0200

    chore: use fastify server instead of server endpoint

--- test/app.e2e.test.ts
+++ test/app.e2e.test.ts
@@ -1,20 +1,32 @@
-const { current } = require('../storage/framework/address.json')
-const request = require('supertest')
+const formidable = require('../.formidable/build').default
+const supertest = require('supertest')
 
 describe('Application (e2e)', () => {
 	let app
 
-	beforeAll(() => app = request(current))
+	beforeAll(async () => {
+		const application = await formidable
+
+		app = application.fastify()
+
+		await app.ready()
+	})
+
+	afterAll(async () => {
+		await app.close()
+	})
 
 	it('/ (GET: Hello World)', () => {
-		app.get('/')
+		supertest(app.server)
+			.get('/')
 			.set('Accept-Language', 'en')
 			.expect(200)
 			.expect('Hello World')
 	})
 
 	it('/ (GET: Hola Mundo)', () => {
-		app.get('/')
+		supertest(app.server)
+			.get('/')
 			.set('Accept-Language', 'es')
 			.expect(200)
 			.expect('Hola Mundo')
