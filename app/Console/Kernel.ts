b80ca3e0dfaa7c2195a807ae1215d537210f00f2
Donald Pakkies <donaldpakkies@gmail.com>
2023-11-18 17:36:14 +0200

    feat: add jobs getter under kernel

--- app/Console/Kernel.ts
+++ app/Console/Kernel.ts
@@ -1,10 +1,17 @@
 import { ConsoleKernel } from '@formidablejs/framework'
 import { Log } from '@formidablejs/logger'
+import { Queueable } from '@formidablejs/queues'
 import type { ICommand } from '@formidablejs/framework'
 import type { Schedule } from '@formidablejs/scheduler'
 
 export class Kernel extends ConsoleKernel {
 	get registered(): Array<ICommand> {
+		return [
+			//
+		]
+	}
+
+	get jobs(): Array<typeof Queueable> {
 		return [
 			//
 		]
