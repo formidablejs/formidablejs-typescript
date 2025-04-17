53b6d7e2162804a41f4e414adb88d44b9aa64044
Donald Pakkies <donaldpakkies@gmail.com>
2025-04-17 12:15:29 +0200

    feat: register UserRepository in shell context

--- app/Resolvers/AppServiceResolver.ts
+++ app/Resolvers/AppServiceResolver.ts
@@ -1,9 +1,16 @@
 import { AuthService as Auth } from '@formidablejs/framework'
 import { ServiceResolver } from '@formidablejs/framework'
+import { UserRepository } from '../Repositories/UserRepository'
 // import { ResetPassword } from '../Mail/ResetPassword'
 // import { VerifyEmail } from '../Mail/VerifyEmail'
 
 export class AppServiceResolver extends ServiceResolver {
+	register(): AppServiceResolver {
+		this.app.context.inject(UserRepository)
+
+		return this
+	}
+
 	boot(): AppServiceResolver {
 		// Auth.verificationMailer(VerifyEmail)
 		// Auth.resetPasswordMailer(ResetPassword)
